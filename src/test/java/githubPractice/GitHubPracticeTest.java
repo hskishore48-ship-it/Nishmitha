b5c87788474e01292a660cfe1792fcdc65638e81
Kishore <Kishore@LAPTOP-V8I65E4R>
2026-01-02 16:43:01 +0530

    First commit

---
+++ src/test/java/githubPractice/GitHubPracticeTest.java
@@ -0,0 +1,15 @@
+package githubPractice;
+
+import org.testng.annotations.Test;
+
+public class GitHubPracticeTest {
+	@Test
+	public void gitHubPRactice() {
+		System.out.println("hii github");
+		
+		String data = System.getProperty("samrty");
+		System.out.println(data);
+		
+	}
+
+}
---
+++ src/test/java/jankinesPractice/JankinesPracticeTest.java
@@ -0,0 +1,11 @@
+package jankinesPractice;
+
+import org.testng.annotations.Test;
+
+public class JankinesPracticeTest {
+	@Test
+	public void jankinesPracticeTest() {
+		System.out.println("hii jankines");
+	}
+
+}
